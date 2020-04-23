beecc36402fa37ed520888400e9d6dd5e90b3ef7
whois-this <46181010+whois-this@users.noreply.github.com>
2020-04-23 11:45:36 +0530

    Clicker Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Clicker from './Clicker'
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,7 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Clicker />
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,7 +19,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
---
+++ src/Clicker.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+class Clicker extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            num : 0,
+            status:false
+        }
+        this.eHandler = this.eHandler.bind(this)
+    }
+
+    eHandler=(e)=>{
+        this.setState((state)=>{
+            let randInt = Math.floor(Math.random()*10)
+            if (randInt===7){
+                state.status = true;
+            }
+            return {num:randInt}
+        })
+        console.log(this.state.num)
+    }
+    render(){
+        return(
+            <div className="Clicker">
+                <h1>{this.state.num}</h1>
+                <div className="Clicker-Button">
+                    {this.state.status?<h1>Game Over</h1>:<button onClick={this.eHandler}>Click Me</button>}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Clicker
